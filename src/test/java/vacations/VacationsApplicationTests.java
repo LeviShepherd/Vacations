8e6c39f00b1a0787ceba7187b809759e69fff947
Levi Shepherd <levi.d.shepherd@outlook.com>
2020-03-12 20:59:48 -0500

    Initial commit

---
+++ src/test/java/vacations/VacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package vacations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
